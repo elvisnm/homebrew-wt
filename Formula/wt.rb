5e9c009864e36be485972dbc32541d0f7b130096
Elvis Moreira <elvis@skulabs.com>
2026-02-27 11:54:42 -0500

    Update wt to 1.0.0 with release checksums

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -7,18 +7,18 @@ class Wt < Formula
   on_macos do
     on_arm do
       url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "6c0990904d946c5d275586fd6307cae6eb080aa0163e360d86bf63cf02322edd"
     end
     on_intel do
       url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "33f5711412532f01bcd5031756b56b1d8d123d427bbda6744dcd7e5c3be15df6"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "acbb52f8a5a38de05955a4b76af82139cd8b2319db3274cf895db699997c97fd"
     end
   end
 
