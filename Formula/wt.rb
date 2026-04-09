606bece3b8dd6b1a3cdbe58e421b1d7d8916991d
Elvis Moreira <contact@elvisnm.com>
2026-04-09 13:58:44 -0400

    Update wt to v1.1.0

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -2,23 +2,23 @@ class Wt < Formula
   desc "Config-driven worktree management toolkit"
   homepage "https://github.com/elvisnm/wt"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_darwin_arm64.tar.gz"
-      sha256 "d33fd9cac9276803132d92ed46c1fd866d2b4409d73d71c49d181bc368c01cc8"
+      url "https://github.com/elvisnm/wt/releases/download/v1.1.0/wt_1.1.0_darwin_arm64.tar.gz"
+      sha256 "b0f235f7e7afffa40dee8d110b521b059e3784f148e36c6ded34a5e1e1ff0c8c"
     end
     on_intel do
-      url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_darwin_amd64.tar.gz"
-      sha256 "6d89fcc5895e8df044d645ff78d4e24d2e0acd0f41e42d9ad383b5db0e188e54"
+      url "https://github.com/elvisnm/wt/releases/download/v1.1.0/wt_1.1.0_darwin_amd64.tar.gz"
+      sha256 "79b780526b63da45e5cae0325753ddc11b2545c4d641285a5b0abab5d20089c9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/elvisnm/wt/releases/download/v1.0.0/wt_1.0.0_linux_amd64.tar.gz"
-      sha256 "12a6375e318d03e74c66c57494d82d2d42643b4dcb77159e3d47d68571bcfdd1"
+      url "https://github.com/elvisnm/wt/releases/download/v1.1.0/wt_1.1.0_linux_amd64.tar.gz"
+      sha256 "4303372ba4a2e0fec05ca614140b163fd364fc162f159842dc35882005a2ad23"
     end
   end
 
